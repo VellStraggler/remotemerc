3b408a62243275b20413fc40a07ca49ad07a13f4
AndrewJuers <juersandrew@gmail.com>
2026-05-27 16:37:28 -0400

    initial landing page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("com.github.javafaker:javafaker:1.0.2")
     implementation("androidx.compose.material:material-icons-extended")
--- app/src/main/java/com/example/remotemerc/MainActivity.kt
+++ app/src/main/java/com/example/remotemerc/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,24 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.remotemerc.ui.theme.RemoteMercTheme
+import com.example.remotemerc.ui.view.LandingPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val fakeDroneRepo = FakeDroneRepo()
-        val fakePeopleRepo= FakePeopleRepo()
-
-        fakeDroneRepo.generateDrones(100)
-        fakePeopleRepo.generatePeople(100)
-
         setContent {
-            val droneViewModel by viewModels<DroneViewModel>()
-
             RemoteMercTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DroneControl(Modifier.padding(innerPadding),
-                        droneViewModel)
+                    LandingPage()
                 }
             }
         }
